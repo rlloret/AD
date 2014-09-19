e72e77ed092286838800ef1c6e9271aeddaaca71
rlloret <robertolloretbelmonte@gmail.com>
2014-09-19 11:33:31 +0200

    19 menu 1

--- PHolaMySql/PHolaMySql/Menu.cs
+++ PHolaMySql/PHolaMySql/Menu.cs
@@ -18,6 +18,30 @@ namespace PHolaMySql
 			seleccion = Console.ReadLine();
 			Console.WriteLine("El texto introducido es: " + seleccion);
 
+
+			switch (seleccion) {
+
+				case "0":
+		
+				break;
+				case "1":
+			
+				break;
+				case "2":
+
+				break;
+				case "3":
+
+				break;
+				case "4":
+
+				break;
+
+
+
+
+			}
+
 		}
 	}
 }
