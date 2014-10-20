706b125f597eb36a834db0ca0eb79d888c63b590
rlloret <robertolloretbelmonte@gmail.com>
2014-10-20 08:58:28 +0200

    Sacado PSerpis a su propia solucion

--- PserpisAd/PserpisAd/Properties/AssemblyInfo.cs
+++ PserpisAd/PserpisAd/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle ("PSerpisAd")]
+[assembly: AssemblyTitle ("PserpisAd")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
